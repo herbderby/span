901c61995c839252d2004be5384b98f8fffd0127
herbderby <herbderby@gmail.com>
2020-06-13 11:15:40 -0400

    Get the compiler to generate more of the code.

--- Span.h
+++ Span.h
@@ -1,23 +1,18 @@
 //
 // Created by Herb Derby on 5/23/20.
 //
-
-#ifndef SPAN_DEFINED
-#define SPAN_DEFINED
-
+#pragma once
+#include <array>
 #include <type_traits>
 
 template<typename T>
 class Span {
 public:
     constexpr Span(T* ptr, size_t size) : ptr_{ptr}, size_{size} {}
-    template <typename U, typename = typename std::enable_if<std::is_same<const U, T>::value>::type>
+    template <typename U, typename = std::enable_if_t<std::is_same_v<const U, T>>>
     constexpr Span(const Span<U>& that) : Span{that.data(), that.size()} {}
-    explicit constexpr Span(const Span& o) = default;
-    constexpr Span& operator=(const Span& that) {
-        new (this) Span<T>{that.data(), that.size()};
-        return *this;
-    }
+    constexpr Span(const Span& o) = default;
+    constexpr Span& operator=(const Span& that) = default;
     template<auto N>
     constexpr Span(T(&a)[N]) : Span{a, N} {}
     template<typename C>
@@ -25,7 +20,7 @@ public:
 
     constexpr T& operator [] (size_t i) const { return ptr_[i]; }
     constexpr T* data() { return ptr_; }
-    constexpr size_t size() const { return size_; }
+    [[nodiscard]] constexpr size_t size() const { return size_; }
 
 private:
     T* ptr_;
@@ -34,5 +29,3 @@ private:
 
 template<typename C>
 Span(C) -> Span<std::remove_pointer_t<decltype(std::data(std::declval<C&>()))>>;
-
-#endif //SPAN_DEFINED
